cf612aa29ca574287eba880f52201db3d71bb9e5
Oleh Pihnastyi <pihnastyi@gmail.com>
2025-11-14 16:07:50 +0100

    PLS-001. Initial commit: Production line simulator

---
+++ src/main/java/com/factory/productionline/ProductionLineApplication.java
@@ -0,0 +1,13 @@
+package com.factory.productionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factory/productionline/ProductionLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.productionline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
